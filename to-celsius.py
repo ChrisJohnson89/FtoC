899b9af141c48eee15dae1fdde454faf376e2f62
Chris Johnson <afckazoo@gmail.com>
2020-05-19 18:52:47 +0700

    Added \n for newline & changed text

--- to-celsius.py
+++ to-celsius.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.6
 
 # Python implementation here
-fahrenheit = float(input("What temperature (in Fahrenheit) would you like converted to Celsius? "))
+fahrenheit = float(input("Enter the current temp (Fahrenheit) to be converted to Celsius:\n"))
 celsius = (fahrenheit - 32) * 5 / 9 
 print(fahrenheit, "F is", celsius, "C")
 
